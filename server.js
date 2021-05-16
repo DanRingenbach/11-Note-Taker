1f29793ee9f14937b4be53c57e798d67cf5d0248
Dan Ringenbach <danringenbach.art@gmail.com>
2021-05-16 14:25:36 -0400

    port fix again

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(apiRoutes)
 app.use(htmlRoutes)
 
 
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
 
